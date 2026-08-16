f6cb9dd71ce6178c7885f828fc0e3868a558cdb1
TBMuktBharatAbhiyaan <tb.mukt.bharat.abhiyaan@gmail.com>
2026-08-16 23:54:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -224,7 +224,7 @@ if (id.length > 0 && !isIdValid) {
             
             // Draw name on PDF (positions doubled)
             pdfCtx.fillStyle = '#000000';
-            pdfCtx.font = `italic ${fontSizes.pdf.name}px "Brush Script MT", cursive, sans-serif`;
+            pdfCtx.font = `italic ${fontSizes.pdf.name}px "Brush Script MT", sans-serif`;
             pdfCtx.textAlign = 'center';
             pdfCtx.fillText(name.toUpperCase(), positions.name.x * 2, positions.name.y * 2);
 
